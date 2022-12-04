3be47d496b96074e8fea10fe8145647dc76c214a
florianlangard <florian.langard@gmail.com>
2022-12-05 00:43:08 +0100

    part one solved

--- advent.php
+++ advent.php
@@ -1,5 +1,27 @@
 <?php
 
 $raw = file_get_contents('data.txt');
-$data = str_replace("\n\n"," ",$raw);
-$data = explode(" ", $data);
+$data = explode("\n", $raw);
+
+foreach ($data as $pair) {
+    $assignements[] = explode(",", $pair);
+}
+
+foreach ($assignements as $assignementPair) {
+    $firstOfPair[] = explode("-", $assignementPair[0]);
+    $secondOfPair[] = explode("-", $assignementPair[1]);
+}
+
+$overlap = 0;
+
+for ($i=0; $i < count($secondOfPair); $i++) { 
+    if (
+        (intval($firstOfPair[$i][0]) <= intval($secondOfPair[$i][0])) && (intval($firstOfPair[$i][1]) >= intval($secondOfPair[$i][1]))
+        ||
+        (intval($secondOfPair[$i][0]) <= intval($firstOfPair[$i][0])) && (intval($secondOfPair[$i][1]) >= intval($firstOfPair[$i][1]))
+        ) 
+        {
+            $overlap++;
+        }
+}
+var_dump($overlap);
